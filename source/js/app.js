08e4b6951d2e124836294c507bd5dd97495b9e72
sckarolos <https://sckarolos@bitbucket.org>
2018-04-02 17:55:52 +0100

    Finalise functionality

--- source/js/app.js
+++ source/js/app.js
@@ -1,39 +1,34 @@
-
 // create the module and name it CodeTaskApp
 var CodeTaskApp = angular.module('CodeTaskApp', ['ngRoute']);
 
 // configure page routes
 CodeTaskApp.config(function($routeProvider) {
   $routeProvider
-
     // route for the home page
     .when('/', {
       templateUrl : 'pages/homePage.html',
       controller  : 'mainController'
     })
-
     // route for the home page
     .when('/home', {
       templateUrl : 'pages/homePage.html',
       controller  : 'mainController'
     })
-
     // route for the about page
     .when('/about', {
       templateUrl : 'pages/secondaryPage.html',
       controller  : 'secondaryController'
     })
-
 });
 
-// create the controller and inject Angular's $scope
+// create the controller and inject Angular's $scope and get factory
 // The following controller should normally be in a separate filechartDirective
-CodeTaskApp.controller('mainController', function($scope, jsonService, $http, $route) {
+CodeTaskApp.controller('mainController', function($scope, jsonService) {
 
-    $scope.items = [];
-    $scope.IsDisplay = false;
-    $scope.titleChart = '';
-
+   $scope.items = [];
+   $scope.IsDisplay = false;
+   $scope.titleChart = '';
+   // get data from json file
    jsonService.getData().then(function(items){
      $scope.items = items.data;
    });
@@ -61,35 +56,29 @@ CodeTaskApp.controller('mainController', function($scope, jsonService, $http, $r
         };
         $scope.items.push(newItem);
       }
-
-      console.log('items are:', $scope.items);
-
    };
 
   // Sample options for first chart
   $scope.chartOptions = {
       title: {
-          text: 'Random data'
+          text: null
       },
-
       chart: {
         width: 320,
         height: 280,
       },
-
       xAxis: {
           categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
               'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
       },
-
       series: [{
-          data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+          data: [29.9, 71.5, 106.4, 119.2, 134.0, 176.0, 135.6, 148.5, 100.4, 124.1, 95.6, 54.4]
       }]
   };
 
-  // Sample data for pie chart
+  // Hardcode line chart Data
   $scope.pieData = [{
-          name: "Microsoft Internet Explorer",
+          name: "Internet Explorer",
           y: 56.33
       }, {
           name: "Chrome",
@@ -108,12 +97,27 @@ CodeTaskApp.controller('mainController', function($scope, jsonService, $http, $r
       }, {
           name: "Proprietary or Undetectable",
           y: 0.2
-  }]
-
+  }];
+  // Create new report
+  $scope.export = function(){
+     html2canvas(document.getElementById('homePageId'), {
+         onrendered: function (canvas) {
+             var data = canvas.toDataURL();
+             var docDefinition = {
+                 content: [{
+                     image: data,
+                     width: 500,
+                 }]
+             };
+             pdfMake.createPdf(docDefinition).download("report.pdf");
+         }
+     });
+  }
 
 });
 
 // The following controller should normally be in a separate file!
+// create the controller and inject Angular's $scope and get factory for secondary controller
 CodeTaskApp.controller('secondaryController', function($scope, jsonService) {
 
   $scope.items = [];
@@ -148,52 +152,62 @@ CodeTaskApp.controller('secondaryController', function($scope, jsonService) {
       $scope.items.push(newItem);
     }
 
-    console.log('items are:', $scope.items);
-
  };
 
-  // Sample options for first chart
+  // Hardcode line chart Data
   $scope.chartOptions = {
       title: {
-          text: 'More random data'
+          text: null
       },
-
       chart: {
         width: 320,
         height: 280,
       },
-
       xAxis: {
           categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
               'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
       },
-
       series: [{
-          data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+          data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 116.4, 194.1, 95.6, 54.4]
       }]
   };
 
-  // Sample data for pie chart
+  // Hardcode pie chart Data
   $scope.pieData = [{
-          name: "Microsoft Internet Explorer",
-          y: 56.33
+          name: "Staging",
+          y: 36.33
       }, {
-          name: "Chrome",
-          y: 24.03,
+          name: "Usage",
+          y: 44.03,
           sliced: true,
           selected: true
       }, {
-          name: "Firefox",
+          name: "Testing",
           y: 10.38
       }, {
-          name: "Safari",
+          name: "Coding",
           y: 4.77
       }, {
-          name: "Opera",
+          name: "QA",
           y: 0.91
       }, {
-          name: "Proprietary or Undetectable",
+          name: "Integration",
           y: 0.2
-  }]
+  }];
+  // Create new report
+  $scope.export = function(){
+     html2canvas(document.getElementById('secondaryPageId'), {
+         onrendered: function (canvas) {
+             var data = canvas.toDataURL();
+             var docDefinition = {
+                 content: [{
+                     image: data,
+                     width: 500,
+                 }]
+             };
+             pdfMake.createPdf(docDefinition).download("report.pdf");
+         }
+     });
+  }
 
 });
