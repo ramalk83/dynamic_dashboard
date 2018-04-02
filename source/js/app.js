11d57d395490e7dd2cb17cc5fea2a12c736c48c4
sckarolos <https://sckarolos@bitbucket.org>
2018-04-02 12:36:36 +0100

    Update chart functionality

--- source/js/app.js
+++ source/js/app.js
@@ -37,6 +37,51 @@ CodeTaskApp.controller('mainController', function($scope, jsonService) {
     console.log('data: ', $scope.items);
   });
 
+  // Sample options for first chart
+  $scope.chartOptions = {
+      title: {
+          text: 'Random data'
+      },
+
+      chart: {
+        width: 320,
+        height: 280,
+      },
+
+      xAxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+              'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      },
+
+      series: [{
+          data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+      }]
+  };
+
+  // Sample data for pie chart
+  $scope.pieData = [{
+          name: "Microsoft Internet Explorer",
+          y: 56.33
+      }, {
+          name: "Chrome",
+          y: 24.03,
+          sliced: true,
+          selected: true
+      }, {
+          name: "Firefox",
+          y: 10.38
+      }, {
+          name: "Safari",
+          y: 4.77
+      }, {
+          name: "Opera",
+          y: 0.91
+      }, {
+          name: "Proprietary or Undetectable",
+          y: 0.2
+  }]
+
+
 });
 
 // The following controller should normally be in a separate file!
@@ -48,5 +93,49 @@ CodeTaskApp.controller('secondaryController', function($scope, jsonService) {
     $scope.items = items.data;
     console.log('data: ', $scope.items);
   });
+
+  // Sample options for first chart
+  $scope.chartOptions = {
+      title: {
+          text: 'More random data'
+      },
+
+      chart: {
+        width: 320,
+        height: 280,
+      },
+
+      xAxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+              'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      },
+
+      series: [{
+          data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+      }]
+  };
+
+  // Sample data for pie chart
+  $scope.pieData = [{
+          name: "Microsoft Internet Explorer",
+          y: 56.33
+      }, {
+          name: "Chrome",
+          y: 24.03,
+          sliced: true,
+          selected: true
+      }, {
+          name: "Firefox",
+          y: 10.38
+      }, {
+          name: "Safari",
+          y: 4.77
+      }, {
+          name: "Opera",
+          y: 0.91
+      }, {
+          name: "Proprietary or Undetectable",
+          y: 0.2
+  }]
 
 });
--- source/js/chartDirective.js
+++ source/js/chartDirective.js
@@ -1,47 +1,53 @@
-            //
-            // .directive('hcChart', function () {
-            //     return {
-            //         restrict: 'E',
-            //         template: '<div></div>',
-            //         scope: {
-            //             options: '='
-            //         },
-            //         link: function (scope, element) {
-            //             Highcharts.chart(element[0], scope.options);
-            //         }
-            //     };
-            // })
-            // // Directive for pie charts, pass in title and data only
-            // .directive('hcPieChart', function () {
-            //     return {
-            //         restrict: 'E',
-            //         template: '<div></div>',
-            //         scope: {
-            //             title: '@',
-            //             data: '='
-            //         },
-            //         link: function (scope, element) {
-            //             Highcharts.chart(element[0], {
-            //                 chart: {
-            //                     type: 'pie'
-            //                 },
-            //                 title: {
-            //                     text: scope.title
-            //                 },
-            //                 plotOptions: {
-            //                     pie: {
-            //                         allowPointSelect: true,
-            //                         cursor: 'pointer',
-            //                         dataLabels: {
-            //                             enabled: true,
-            //                             format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-            //                         }
-            //                     }
-            //                 },
-            //                 series: [{
-            //                     data: scope.data
-            //                 }]
-            //             });
-            //         }
-            //     };
-            // })
+
+var CodeTaskApp = angular.module('CodeTaskApp');
+    // Directive for generic chart, pass in chart options
+    CodeTaskApp.directive('hcChart', function () {
+        return {
+            restrict: 'E',
+            template: '<div></div>',
+            scope: {
+                options: '='
+            },
+            link: function (scope, element) {
+                Highcharts.chart(element[0], scope.options);
+            }
+        };
+    })
+
+    // Directive for pie charts, pass in title and data only
+    // Directive for pie charts, pass in title and data only
+    .directive('hcPieChart', function () {
+        return {
+            restrict: 'E',
+            template: '<div></div>',
+            scope: {
+                title: '@',
+                data: '='
+            },
+            link: function (scope, element) {
+                Highcharts.chart(element[0], {
+                    chart: {
+                        type: 'pie'
+                    },
+                    title: {
+                        text: scope.title
+                    },
+                    plotOptions: {
+                        pie: {
+                            size: "50%",
+                            center: ['62%', '30%'],
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: true,
+                                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                            }
+                        }
+                    },
+                    series: [{
+                        data: scope.data
+                    }]
+                });
+            }
+        };
+    })
